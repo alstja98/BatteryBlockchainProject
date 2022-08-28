2a619bdd88defec2e5c599406fabfb58b3320d5d
tnsqjahong <cruseight47@naver.com>
2022-08-28 18:21:49 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import { useMoralis } from "react-moralis";
+import "./App.css";
+import Mainpage from "./pages/Mainpage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Mainpage />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Mainpage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useMoralis } from "react-moralis";
+
+const Mainpage = () => {
+  const { isAuthenticated, Moralis } = useMoralis();
+
+  return <></>;
+};
+
+export default Mainpage;
